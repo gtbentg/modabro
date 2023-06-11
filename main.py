beff3ab0bf22a7261937b4395b78571acb3c2f83
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-11 23:24:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+import pyrogram
+
+import requests
+
+import time
+
+def get_new_releases():
+
+    response = requests.get("https://imdb-api.com/en/API/TopRatedTV/k_ouua140i")
+
+    try:
+
+        data = response.json()
+
+        new_releases = []
+
+        for item in data['items']:
+
+            title = item['title']
+
+            release_date = item['year']
+
+            new_releases.append((title, release_date))
+
+        return new_releases
+
+    except ValueError:
+
+        print("Error decoding response as JSON:")
+
+        print(response.content)
+
+        return []
+
+bot = pyrogram.Client("my_bot", api_id=15428219, api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", bot_token="5310839293:AAFf3gugWXvL3_vBpumyxcaC0VovBZ-TbuY")
+
+@bot.on_message()
+
+async def handle_message(client, message):
+
+    if message.text == "/start":
+
+        await message.reply_text(
+
+            "Welcome to the new movie and series release bot! I will send you a list of new releases every day."
+
+        )
+
+async def send_new_releases():
+
+    await bot.start()  # Start the Pyrogram client
+
+    new_releases = get_new_releases()
+
+    for title, release_date in new_releases:
+
+        await bot.send_message(chat_id="YOUR_CHAT_ID", text=f"New release: {title} ({release_date})")
+
+    while True:
+
+        time.sleep(86400)  # Wait for 24 hours
+
+        new_releases = get_new_releases()
+
+        for title, release_date in new_releases:
+
+            await bot.send_message(chat_id="YOUR_CHAT_ID", text=f"New release: {title} ({release_date})")
+
+bot.run(send_new_releases())
+
