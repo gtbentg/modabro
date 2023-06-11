e92ad84db93c1b7a01880fd1c4e5137c21a2953b
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-11 23:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def send_new_releases():
 
     for title, release_date in new_releases:
 
-        await bot.send_message(chat_id="YOUR_CHAT_ID", text=f"New release: {title} ({release_date})")
+        await bot.send_message(chat_id="-1001649665241", text=f"New release: {title} ({release_date})")
 
     while True:
 
@@ -64,7 +64,7 @@ async def send_new_releases():
 
         for title, release_date in new_releases:
 
-            await bot.send_message(chat_id="YOUR_CHAT_ID", text=f"New release: {title} ({release_date})")
+            await bot.send_message(chat_id="-1001649665241", text=f"New release: {title} ({release_date})")
 
 bot.run(send_new_releases())
 
