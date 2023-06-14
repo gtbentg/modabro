dfcd7d98a8fd035f6f69d6e2eb2dfaa486783dda
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-15 00:45:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 # Create a new Telegram bot using the API token
 
-bot = Client("my_bot", api_id="15428219", api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", bot_token="5507296374:AAHzdrj_nru8XQbNtRSAraVQ3eJd6r3HIC4")
+bot = Client("my_bot", api_id="15428219", api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", bot_token="5507296374:AAFiZjhKFelrMNXXUoJe8EkjHvua5QeV2q0")
 
 @bot.on_message(filters.document)
 
@@ -16,9 +16,11 @@ def delete_documents(bot, message):
 
         if updated_caption != caption:
 
-            bot.edit_message_caption(chat_id=message.chat.id, message_id=message.message_id, caption=updated_caption)
+            original_message_id = message.reply_to_message.message_id
+
+            bot.edit_message_caption(chat_id=message.chat.id, message_id=original_message_id, caption=updated_caption)
 
 # Run the bot
-print("ok💝")
+
 bot.run()
 
