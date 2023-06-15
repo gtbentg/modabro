3d73000543d11d7697fd4ed5b42de80dab38c384
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-15 10:29:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyrogram
 
-bot = pyrogram.Client("my_bot")
+bot = pyrogram.Client("my_bot", api_id="15428219", api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", bot_token="5507296374:AAFiZjhKFelrMNXXUoJe8EkjHvua5QeV2q0")
 
 @bot.on_message()
 
@@ -29,3 +29,6 @@ async def handler(message):
         # Send the file back to the sender
 
         await message.reply_document(message.document)
+                                  
+print("🙌🏻🙌🏻")
+bot.run()                          
