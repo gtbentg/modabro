bb1ee63ebdce27bbe55fb574e572a7cef479403c
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-15 15:11:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,26 @@
 import pyrogram
 
-# Get your API ID and hash from https://core.telegram.org/api/obtaining_api_id
-
 api_id = "15428219"
 
 api_hash = "0042e5b26181a1e95ca40a7f7c51eaa7"
 
 bot_token = "5507296374:AAFiZjhKFelrMNXXUoJe8EkjHvua5QeV2q0"
 
-# Create a Pyrogram client
-
-client = pyrogram.Client(
-
-    "my_bot",
-
-    api_id=api_id,
-
-    api_hash=api_hash,
-
-    
-
-    bot_token=bot_token
-
-)
-
-# Define a function that replaces the text "@CinimaAdholokaam" and "@Calinkzz" with "@MovieBossTG" in a caption
-
-def replace_text(caption):
-
-    caption = caption.replace("@CinimaAdholokaam", "@MovieBossTG")
-
-    caption = caption.replace("@Calinkzz", "@MovieBossTG")
-
-    return caption
-
-# Listen for messages
+client = pyrogram.Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @client.on_message()
 
 async def handle_message(client, message):
 
-    # Check if the message is a file with a caption
+    if message.document is not None:
 
-    if message.document and message.caption:
+        caption = message.caption
 
-        # Replace the text "@CinimaAdholokaam" and "@Calinkzz" with "@MovieBossTG" in the caption
+        if "@CinimaAdholokaam" in caption or "@Calinkzz" in caption:
 
-        caption = replace_text(message.caption)
+            new_caption = caption.replace("@CinimaAdholokaam", "@MovieBossTG").replace("@Calinkzz", "@MovieBossTG")
 
-        # Send the new caption along with the file
-
-        await message.reply_document(message.document, caption=caption)
-
-# Run the bot
-
-print("🙌🏻💝💝🙌🏻")
+            await message.edit(caption=new_caption)
 
 client.run()
 
